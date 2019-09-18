fb1400c5d889289e1c3ee76be7bba71a2cb90617
Brendan McGhee <b.mcghee.1101@gmail.com>
2019-09-17 22:13:46 -0500

    Added data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,8 +8,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        } else {            
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.\nHigh: ' + body.daily.data[0].temperatureHigh + ' Low: ' + body.daily.data[0].temperatureLow)
         }
     })
 }
